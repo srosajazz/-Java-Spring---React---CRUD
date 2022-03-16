0c6a27fce5a98c5ef6199148ecf75bd8fbc1990a
Sergio Rosa <scarlosjazz@gmail.com>
2022-03-16 18:34:58 -0400

    add: H2 databas & layers

--- backend/src/main/java/com/sergiorosa/crudcatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/sergiorosa/crudcatalog/resources/CategoryResource.java
@@ -1,25 +1,27 @@
 package com.sergiorosa.crudcatalog.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sergiorosa.crudcatalog.entities.Category;
+import com.sergiorosa.crudcatalog.services.CategoryService;
+
 
 @RestController
 @RequestMapping(value = "/categories")
-public class CategoryResource { 
+public class CategoryResource {
+	
+	@Autowired
+	private CategoryService service;
 	
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll(){	
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Hotels"));
-		list.add(new Category(2L, "Car Rental"));
+		List<Category> list = service.findAll();
 		return ResponseEntity.ok().body(list);
    }
-
 }
---
+++ backend/src/main/java/com/sergiorosa/crudcatalog/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.sergiorosa.crudcatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sergiorosa.crudcatalog.entities.Category;
+import com.sergiorosa.crudcatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+}
