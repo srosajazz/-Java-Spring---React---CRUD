50f71a50e31d34e34598e223e0cb67700b424c58
Sergio Rosa <scarlosjazz@gmail.com>
2022-03-16 08:49:44 -0400

    Backend project created

---
+++ backend/src/main/java/com/sergiorosa/crudcatalog/CrudcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.sergiorosa.crudcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sergiorosa/crudcatalog/CrudcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiorosa.crudcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
