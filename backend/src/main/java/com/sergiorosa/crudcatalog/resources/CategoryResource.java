e940fb39a43cd6a1eca78d00df0eb862281d9504
Sergio Rosa <scarlosjazz@gmail.com>
2022-03-17 16:46:59 -0400

    POST new Category

--- backend/src/main/java/com/sergiorosa/crudcatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/sergiorosa/crudcatalog/resources/CategoryResource.java
@@ -1,13 +1,17 @@
 package com.sergiorosa.crudcatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sergiorosa.crudcatalog.DTO.CategoryDTO;
 import com.sergiorosa.crudcatalog.services.CategoryService;
@@ -31,4 +35,27 @@ public class CategoryResource {
 		CategoryDTO  dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
    }
+	
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
