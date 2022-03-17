5815b71e2d1a1bc51c5d30c02ab6ae4473ac47f5
Sergio Rosa <scarlosjazz@gmail.com>
2022-03-17 09:02:09 -0400

    DTO

--- backend/src/main/java/com/sergiorosa/crudcatalog/services/CategoryService.java
+++ backend/src/main/java/com/sergiorosa/crudcatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.sergiorosa.crudcatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.sergiorosa.crudcatalog.DTO.CategoryDTO;
 import com.sergiorosa.crudcatalog.entities.Category;
 import com.sergiorosa.crudcatalog.repositories.CategoryRepository;
 
@@ -16,7 +18,9 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		//Lambda expression
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 }
